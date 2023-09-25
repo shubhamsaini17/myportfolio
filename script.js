96dba869161140d19c18bc6bf6212947be2c252a
shubham <103417842+shubhamsaini17@users.noreply.github.com>
2023-09-25 06:29:25 +0530

    script.js

--- script.js
+++ script.js
@@ -22,6 +22,21 @@ function opentab(tabname) {
 
 
 
+// cursor
+var crsr= document.querySelector("#cursor");
+var blur= document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",function(dets){
+   
+    crsr.style.left = dets.x +"px"
+    crsr.style.top = dets.y +"px"
+    blur.style.left = dets.x- 50+"px"
+    blur.style.top = dets.y -50+"px"
+})
+
+
+
+
+
 
 
 

69d09acbce7a7fe927345c51df6aa2bc2f69046e
shubham <103417842+shubhamsaini17@users.noreply.github.com>
2023-10-11 18:25:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+function startTypedAnimation() {
+    var typed = new Typed('#element', {
+      strings: ['Web Developer', 'Ethical Hacker', 'Web Designer', 'Penetration Tester'],
+      typeSpeed: 50,
+      backSpeed: 25, // Added backSpeed to simulate erasing text
+      startDelay: 100, // Delay before starting the animation
+      backDelay: 800, // Delay before erasing text
+      loop: true, // Infinite loop
+    });
+  }
 
-
-var typed = new Typed('#element', {
-    strings: ['Web Developer', 'Ethical Hacker', ' Penetration Tester'],
-    typeSpeed: 50,
-});
+  startTypedAnimation();
 // ----------------------------------------
 var tablinks = document.getElementsByClassName("tab-links")
 var tabcontents = document.getElementsByClassName("tab-contents")
@@ -34,7 +40,35 @@ document.addEventListener("mousemove",function(dets){
 })
 
 
+// Setup and start animation! 
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxR5x6R0TYGpPN1D1I19e7IN1lkLCRGMFhua9j099aeMcTurYPKR3UPODs_RutzEIdd/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML= "Message sent sucessfully"
+            setTimeout(function(){
+                msg.innerHTML= ""   
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
 
+
+    // sidemenu for mobile
+
+    var sidemenu = document.getElementById("sidemenu");
+
+    function openmenu(){
+        sidemenu.style.right ="0";
+    }
+    function closemenu(){
+        sidemenu.style.right ="-200px";
+    }
 
 
 
